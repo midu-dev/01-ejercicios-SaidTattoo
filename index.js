c5640d612af723470345c5450ba29a023092cfdd
Said Ravest (SaidTattoo) <said.tattoo@gmail.com>
2023-07-24 14:44:00 -0400

    primera prueba resuelta

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+const fs = require('node:fs')
+const path = require('node:path')
 // Ejercicio 2
-async function writeFile (filePath, data, callback) {
-
+async function writeFile(filePath, data, callback) {
+  const dirPath = path.dirname(filePath)
+  try {
+    await fs.promises.mkdir(dirPath, { recursive: true })
+    await fs.promises.writeFile(filePath, data)
+    callback()
+  } catch (err) {
+    callback(err)
+  }
 }
 
-// Ejercicio 3
-async function readFileAndCount (word, callback) {
 
+// Ejercicio 3
+async function readFileAndCount(word, callback) {
+  if (!word) {
+    const error = new Error('No se ha especificado la palabra a buscar')
+    return callback(error)
+  }
+  const filePath = process.argv[2]
+  if (!filePath) {
+    const error = new Error('No se ha especificado el path del archivo')
+    return callback(error)
+  }
+
+ try {
+    await fs.promises.access(filePath, fs.constants.F_OK);
+    const content = await fs.promises.readFile(filePath, 'utf-8');
+    const existWord = (content.match(new RegExp(word, 'gi')) || []).length;
+    callback(null,existWord);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      callback(null,0); // Si el archivo no existe, devolvemos 0 como resultado
+    }else{
+      callback(err);
+    }
+  }
 }
 
 module.exports = {
